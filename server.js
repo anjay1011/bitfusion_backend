1f121cd17b26d2e6792aa35d70a75f72813c6b56
Ayush_Anjay <ayush2310125@akgec.ac.in>
2025-09-15 23:36:52 +0530

    backend #3

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(
     origin: [
               process.env.FRONTEND_URL,
               "http://localhost:3000",
+              "http://localhost:9002",
               "https://traffic-flow-ttpx.vercel.app",
             ],
     methods: ["GET", "POST"],
